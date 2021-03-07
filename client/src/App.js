b70e19fc893188e3dd45d2727451d14082341e0b
devmshankar <devmshankar@gmail.com>
2021-03-08 02:26:12 +0530

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/login';
+
 function App() {
 	return (
-		<div className="app h-screen w-screen bg-black text-white">
-			<h1>Hello react app</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/">
+					<div className="app h-screen w-screen bg-black text-white">
+						<h1>Hello react app</h1>
+					</div>
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
